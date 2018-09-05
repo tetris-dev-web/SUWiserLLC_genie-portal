4566f3ab1c2db66c41d87c48691a1dcab9c52d75
Chris <chrislee92@gmail.com>
2018-09-05 15:06:45 -0400

    change class name

--- db/migrate/20180904214052_change_cost_to_revenue.rb
+++ db/migrate/20180904214052_change_cost_to_revenue.rb
@@ -1,4 +1,4 @@
-class ChangerevenueToRevenue < ActiveRecord::Migration[5.1]
+class ChangeCostToRevenue < ActiveRecord::Migration[5.1]
   def change
     rename_column :projects, :revenue, :revenue
   end
