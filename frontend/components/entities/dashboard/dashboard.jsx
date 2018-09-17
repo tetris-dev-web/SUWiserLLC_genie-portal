4de8fe6c0498f673b6b86ac7a5367947d1d2a254
matsteele <matsteele@gmail.com>
2018-09-17 14:51:13 -0400

    stage changes

--- frontend/components/entities/dashboard/dashboard.jsx
+++ frontend/components/entities/dashboard/dashboard.jsx
@@ -4,11 +4,7 @@ import ProjectDashboardContainer from './project_dashboard/project_graph_contain
 import DrizzleConsumer from '../../drizzle/drizzleConsumer';
 import ReadString from './readString';
 import ReadStringContainer from './readStringContainer';
-<<<<<<< HEAD
-=======
 import SetString from './SetString';
->>>>>>> 823b62caefe08249d8b48ed6b6f00f3717cfa0fb
-import SetString from './setString';
 import {drizzleConnect} from 'drizzle-react';
 import PropTypes from 'prop-types'
 
