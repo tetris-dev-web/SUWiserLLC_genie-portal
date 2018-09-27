dcfaf3313358751a475ea9e6df097af879ed0aa3
george najm <georgenajm28@yahoo.com>
2018-09-27 16:34:58 -0400

    fixed styling

--- frontend/components/entities/dashboard/project_dashboard/project_thermo.jsx
+++ frontend/components/entities/dashboard/project_dashboard/project_thermo.jsx
@@ -5,17 +5,26 @@ import { Stage, Layer, Rect,Line, Text } from 'react-konva';
 const ProjectThermo = function( {project} ) {
   const { current_capital,capital_required } = project;
   const percentCompleted = (current_capital*100) / capital_required;
-  const rectWidth = 20;
+  const rectWidth = 22;
   const rectHeigth = 110;
-  const filledRectHeigth = (percentCompleted/100) * rectHeigth ;
-  debugger
+  const filledRectHeigth = ( percentCompleted/100 ) * rectHeigth;
+  const filledRectStartingY = ( rectHeigth+20 ) - ( filledRectHeigth+1 );
   return (
     <Stage width={200} height={200}>
       <Layer>
         <Line
-          points={[35,27,157,27]}
+          points={[35,filledRectStartingY,190,filledRectStartingY]}
           stroke={'white'}
-          strokeWidth={2}
+          strokeWidth={1.5}
+          />
+        <Rect
+          x={21}
+          y={ filledRectStartingY }
+          width={ rectWidth-2 }
+          height={ filledRectHeigth }
+          cornerRadius={9}
+          fill={'#223562'}
+          shadowBlur={5}
           />
         <Rect
           x={20}
@@ -23,63 +32,13 @@ const ProjectThermo = function( {project} ) {
           width={ rectWidth }
           height={ rectHeigth }
           cornerRadius={9}
-          fill={'black'}
+
           stroke={'white'}
           strokeWidth={1}
-          shadowBlur={5}
-          />
-        <Rect
-          x={21}
-          y={20}
-          width={ rectWidth-2 }
-          height={ filledRectHeigth }
-          cornerRadius={9}
-          fill={'#223562'}
           shadowBlur={5}
           />
       </Layer>
     </Stage>
   );
 };
-
-// class ProjectThermo extends React.Component {
-//
-//   render() {
-//     return (
-//       <Stage width={200} height={200}>
-//         <Layer>
-//           <Line
-//             points={[35,27,157,27]}
-//             stroke={'white'}
-//             strokeWidth={2}
-//             />
-//           <Rect
-//             x={20}
-//             y={20}
-//             width={20}
-//             height={110}
-//             cornerRadius={9}
-//             fill={'black'}
-//             stroke={'white'}
-//             strokeWidth={1}
-//             shadowBlur={5}
-//             />
-//           <Rect
-//             x={21}
-//             y={20}
-//             width={18}
-//             height={109}
-//             cornerRadius={9}
-//             fill={'#223562'}
-//             shadowBlur={5}
-//             />
-//         </Layer>
-//       </Stage>
-//     );
-//   }
-// }
-// fill={'#223562'}
-// fillLinearGradientEndPointX={20}
-// fillLinearGradientEndPointY={65}
-// fillLinearGradientColorStops={[0, 'black', 1, '#223562']}
 export default ProjectThermo;
