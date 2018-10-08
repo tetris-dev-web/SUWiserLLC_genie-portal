3570916f7872c70b99c54e349ee2417485835224
george najm <georgenajm28@yahoo.com>
2018-10-08 12:01:18 -0400

    fixed scales on thermo

--- frontend/components/entities/dashboard/project_dashboard/project_thermo.jsx
+++ frontend/components/entities/dashboard/project_dashboard/project_thermo.jsx
@@ -22,19 +22,19 @@ const ProjectThermo = function( {project} ) {
   const formattedStartDate = new Date(start_date);
   const oneDay = 24*60*60*1000;
   const daysToClose = Math.round(Math.abs((formattedCloseDate.getTime() - timeNow.getTime())/(oneDay)));
+  const jsonVotes = JSON.parse(project.votes);
+
   const xTimeScale = d3.scaleTime()
                    .domain([formattedStartDate,formattedCloseDate])
                    .range([rectStartingX + rectWidth + 60, rectStartingX + 130]).clamp(true);
   const yVoteScale = d3.scaleLinear()
-                   .domain([1,capital_required])
+                   .domain([ 1,d3.max(Object.values(jsonVotes)) ])
                    .range([rectHeigth+rectStartingY, filledRectStartingY]).clamp(true);
 
   const daysToCloseLineX = xTimeScale(timeNow);
   const voteXScale = d3.scaleTime()
                     .domain([formattedStartDate,formattedCloseDate])
                     .range([rectStartingX+rectWidth,daysToCloseLineX]).clamp(true);
-  const jsonVotes = JSON.parse(project.votes);
-
   const sortedVotesByDate = Object.keys(jsonVotes).sort((a,b)=>{
     const date1 = new Date(a);
     const date2 = new Date(b);
@@ -93,10 +93,10 @@ const ProjectThermo = function( {project} ) {
           strokeWidth={1}
           />
         <Text
-          x={daysToCloseLineX + 5}
-          y={100}
-          text={ daysToClose + ' days to close' }
-          fontSize={13}
+          x={daysToCloseLineX - 5}
+          y={168}
+          text={ `${daysToClose} days \n till close` }
+          fontSize={12}
           fontFamily={'open sans condensed'}
           fill={'#008080'}
           strokeWidth={1}
@@ -124,18 +124,20 @@ const ProjectThermo = function( {project} ) {
             sceneFunc={(context, shape) => {
               context.beginPath();
               context.moveTo(daysToCloseLineX, filledRectStartingY);
-              context.lineTo(rectStartingX, filledRectStartingY);
-              context.lineTo(rectStartingX, rectHeigth+rectStartingY);
+              context.lineTo(rectStartingX - 1, filledRectStartingY);
+              context.lineTo(rectStartingX - 1, rectHeigth+rectStartingY);
               context.lineTo(rectStartingX+rectWidth, rectHeigth+rectStartingY);
               let sumVotes = 0;
               sortedVotesByDate.forEach(date=>{
                 const formattedDate = new Date(date);
+                debugger
                 const x = voteXScale(formattedDate);
                 sumVotes += jsonVotes[date];
                 const y = yVoteScale(sumVotes);
+                console.log("x:",x);
                 context.lineTo(x,y);
               });
-              context.closePath();
+              // context.closePath();
               context.fillStrokeShape(shape);
             }}
             fill="#008080"
