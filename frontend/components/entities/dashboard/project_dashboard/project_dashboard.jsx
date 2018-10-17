622fc8e07634b0aa652d15908d6ab4babe250a21
george najm <georgenajm28@yahoo.com>
2018-10-17 14:17:57 -0400

    rm title from description text

--- frontend/components/entities/dashboard/project_dashboard/project_dashboard.jsx
+++ frontend/components/entities/dashboard/project_dashboard/project_dashboard.jsx
@@ -90,9 +90,6 @@ class ProjectDashboard extends React.Component {
 
                     <div className="project-description">
                       <div className="project-text">
-                        <div className="title-wrapper">
-                          <h1 className="text-hidden">{projectClicked.title}</h1>
-                        </div>
                         <div className="project-summary">
                           {projectClicked.summary}
                         </div>
