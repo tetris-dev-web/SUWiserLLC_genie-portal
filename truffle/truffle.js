2e015a07a76ace9b25dfdb6da0a7df0ac8a268b3
Rhossan <rhossan93@gmail.com>
2018-10-24 16:26:58 -0400

    changes before pull

--- truffle/truffle.js
+++ truffle/truffle.js
@@ -13,8 +13,8 @@
  */
 const HDWalletProvider = require("truffle-hdwallet-provider");
 require('dotenv').config();
-
-var HDWalletProvider = require("truffle-hdwallet-provider");
+//
+// var HDWalletProvider = require("truffle-hdwallet-provider");
 
 var infura_apikey = "32d629913620458ea1049490da4906b1";
 var mnemonic = "leaf oak make armed infant basic cup music beauty police urban double";
@@ -29,22 +29,19 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-<<<<<<< HEAD
-      gas: 4600000
+      gasLimit: 10000000
     },
     ropsten: {
          provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY),
          network_id: 3,
-         gas: 5000000  
-      }
+         gas: 4500000,
+         gasPrice: 10000000000
+      },
+      solc: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
   }
 };
-=======
-      gamLimit: 10000000
-    }
-  }
-};
-
-
-//4f879311fa96bf6cce1ec675c47a53845b1b8e28 - public address
->>>>>>> a369a916a52f4a344302892be936624e2ecfad5f
