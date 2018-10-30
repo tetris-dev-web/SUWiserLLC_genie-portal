7c617f5b9107ce8eead59c46260dedbdc8acb7ba
Ryan <rnowusu@gwu.edu>
2018-10-30 14:56:22 -0400

    staged dizzleConsumer

--- frontend/components/drizzle/drizzleConsumer.jsx
+++ frontend/components/drizzle/drizzleConsumer.jsx
@@ -7,23 +7,6 @@ export default ({component: Component, props, actions}) => (
   <DrizzleContext.Consumer>
     {drizzleContext => {
       const { drizzle, drizzleState, initialized } = drizzleContext;
-<<<<<<< HEAD
-
-      // debugger window.web3 === undefined || this.props.web3.status === 'failed'
-      // if (window.web3 === undefined) {
-      //   return(
-      //     // Display a web3 warning.
-      //     <div className="warning">
-      //       <p>This browser has no connection to the Ethereum network. </p>
-      //       <p>Please use the Chrome/FireFox extension MetaMask, or dedicated Ethereum browsers Mist or Parity.</p>
-      //     </div>
-      //   );
-      // }
-
-
-
-=======
->>>>>>> ae5d08597b9ee2831d1ef3eed814e032883a9003
 
       if (!initialized) {
         return "Loading...";
