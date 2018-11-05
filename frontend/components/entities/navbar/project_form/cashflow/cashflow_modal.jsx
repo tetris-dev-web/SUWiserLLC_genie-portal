4741159a14b0fa1c52fb9e0dac4fc98df16f7b0b
mjohnson324 <mjohnson324@gmail.com>
2018-11-04 19:32:06 -0500

    Add cashflow modal and thumbs-up display for pitching projects

---
+++ frontend/components/entities/navbar/project_form/cashflow/cashflow_modal.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Modal from 'react-modal';
+import CashFlow from './cashflow';
+import ModalStyle from './modal_style';
+
+class CashFlowModal extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { openModal: false };
+
+    window.SessionOpenModal = () => {
+      this.setState({openModal: true});
+    };
+
+    window.SessionOpenModal = window.SessionOpenModal.bind(this);
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  // solves 'Warning: react-modal: App element is not defined'
+  UNSAFE_componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  openModal() {
+    this.setState({openModal: true});
+  }
+
+  closeModal() {
+    this.setState({openModal: false});
+  }
+
+  render() {
+
+    return (
+      <div onClick={this.openModal}>
+        Cashflows
+        <Modal
+          isOpen={this.state.openModal}
+          onRequestClose={this.closeModal}
+          style={ModalStyle}
+          contentLabel="Manage Modal"
+          className="modal-container">
+          <CashFlow closeModal={this.closeModal} />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default CashFlowModal;
