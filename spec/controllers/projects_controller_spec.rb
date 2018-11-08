0c0e0e9931fa7716128dedf130b3588c5bd51175
Ryan <rnowusu@gwu.edu>
2018-11-08 18:00:16 -0500

    added rspec, added collection route to projects controller, implemented logic to extract certain cashflows prerefractoring. Will change

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Api::ProjectsController, type: :controller do
+  # @expected = -266505#.to_json
+  # before do
+  #   get :deployed_project_revenue, format: :json
+  # end
+  # let(:project) { Project.first }
+  #
+  # describe 'GET #deployed_project_revenue' do
+  #   it { should route(:get, '/projects/deployed_project_revenue').to(action: :deployed_project_revenue) }
+  # end
+  # it "should be the lowest number in the array" do
+  #
+  # end
+
+end
