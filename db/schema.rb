559b371e50abf01b403bcfc5eea378dbb9660575
Chris <chrislee92@gmail.com>
2018-11-27 13:00:05 -0500

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20181030163518) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "tokens"
+    t.boolean "user_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
