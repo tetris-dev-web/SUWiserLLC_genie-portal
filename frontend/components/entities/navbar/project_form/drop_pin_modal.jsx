2f1b7c8bdc7584368b2b51783e95387195773574
Liangtian Zhang <lzhang40@binghamton.edu>
2018-12-11 13:10:53 -0500

    added icons for project form buttoms

--- frontend/components/entities/navbar/project_form/drop_pin_modal.jsx
+++ frontend/components/entities/navbar/project_form/drop_pin_modal.jsx
@@ -31,7 +31,7 @@ class DropPinModal extends React.Component {
   render() {
     return (
       <div>
-          <svg className="project-form-button-icons" viewBox="0 0 100 100" onClick={this.openModal}><path d="M53,55.7a19.5,19.5,0,1,0-6,0V77h0c0,1.7,1.3,5.9,3,5.9s3-4.2,3-5.9h0ZM50,50A13.5,13.5,0,1,1,63.5,36.5,13.6,13.6,0,0,1,50,50Z" /></svg>
+        <svg className="project-form-button-icons" viewBox="0 0 100 100" onClick={this.openModal}><path d="M53,55.7a19.5,19.5,0,1,0-6,0V77h0c0,1.7,1.3,5.9,3,5.9s3-4.2,3-5.9h0ZM50,50A13.5,13.5,0,1,1,63.5,36.5,13.6,13.6,0,0,1,50,50Z" /></svg>
         <Modal
           isOpen={this.state.openModal}
           onRequestClose={this.closeModal}
