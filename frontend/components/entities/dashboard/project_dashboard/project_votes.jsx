6638ad874f5e3fb9b71adfe12c95783b0aeb849c
Steven <stvtortora@gmail.com>
2018-12-11 18:27:51 -0500

    create graph toggle

---
+++ frontend/components/entities/dashboard/project_dashboard/project_votes.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ProjectVotes extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>Project votesssssssssss ma nigggggaaaaaa</div>
+    )
+  }
+}
+
+export default ProjectVotes;
