b22a92377748fa65f5a83b4bca982230402c6e78
Ryan <rnowusu@gwu.edu>
2018-12-13 18:14:31 -0500

    removed console logs, fixed bug with binded function, tweaked discount factor function, worked on poly window

--- frontend/components/entities/navbar/project_form/project_form.jsx
+++ frontend/components/entities/navbar/project_form/project_form.jsx
@@ -64,7 +64,7 @@ class ProjectForm extends React.Component {
     this.storeAddress = this.storeAddress.bind(this);
     this.calculateCapitalRequired = this.calculateCapitalRequired.bind(this);
     this.parseInputFile = this.parseInputFile.bind(this);
-    this.updateCashflowValue = this.update.bind(this);
+    this.updateCashflowValue = this.updateCashflowValue.bind(this);
     this.updateActuals = this.updateActuals.bind(this);
   }
 
@@ -204,11 +204,11 @@ class ProjectForm extends React.Component {
   calculateDiscountFactor(){
     // console.log("Failed Projects are: ", this.getFailedProjects());
     let capitalDeployed = this.calculateTotalCapitalDeployed();
-    let discountFactor = (50 - ((capitalDeployed/190000.0) + (this.getFailedProjects() * 6)));
-    if (discountFactor > 10) {
+    let discountFactor = (.50 - ((capitalDeployed/190000.0) + (this.getFailedProjects() * 0.06)));
+    if (discountFactor > .10) {
       return discountFactor;
     } else {
-      return 10;
+      return .10;
     }
   }
 
@@ -255,6 +255,7 @@ class ProjectForm extends React.Component {
 
   updateCashflowValue(quarter) {
     // console.log("Type of quarter is: ", typeof quarter);
+    console.log("Is the cashflow value being updated?");
     return e => {
       e.preventDefault();
       let cashflow = merge({}, this.state.cashflow);
@@ -385,13 +386,13 @@ class ProjectForm extends React.Component {
   }
 
   render() {
-    // console.log("Finance is ", Finance);
-    // console.log("Finance.NPV is ", new Finance().NPV);
-    // console.log("Net Present Value is: ", this.calculateNetPresentValue(Object.values(processCashData(this.props.projects))[0].cashflow));
-    // console.log("Discount factor is: ", this.calculateDiscountFactor());
-    // console.log("Capital is: ", this.calculateTotalCapitalDeployed());
-    // console.log("Cashflow is: ", this.state.cashflow);
-    // console.log("Cap required is: ", this.calculateCapitalRequired() * -1);
+
+    let modelLink;
+    if (this.state.modelId === "") {
+      modelLink = "https://poly.google.com"
+    } else {
+      modelLink = "https://poly.google.com/view/" + this.state.modelId
+    }
 
     const geojsons = [];
     const fileId = ["file1", "file2", "file3", "file4", "file5"];
@@ -523,8 +524,8 @@ class ProjectForm extends React.Component {
           <div className="cap-row">
             <span>valuation</span>
 
-            <div className="style2">{this.state.projected_cashflow ? this.calculateNetPresentValue(Object.values(this.state.projected_cashflow).slice(this.state.currentQuarter - 1)) : '$830,000'}</div>
-            <div className="style2">{this.state.accumulatedRevenue ? this.calculateCapitalRequired() : "$130,000"}</div>
+            <div className="style2">{this.state.projected_cashflow ? "$" + this.calculateNetPresentValue(Object.values(this.state.projected_cashflow).slice(this.state.currentQuarter - 1)) : '$830,000'}</div>
+            <div className="style2">{this.state.accumulatedRevenue ? "$" + this.calculateCapitalRequired() : "$130,000"}</div>
             <span>capital <br />  required</span>
           </div>
 
@@ -572,10 +573,11 @@ class ProjectForm extends React.Component {
           </div>
 
           <DivWithCorners>
-            <PolyModal
-              modelId={this.state.modelId}
-
-              />
+            <div className="project-form-button model-id">
+              <a href={modelLink} target="_blank">
+                <svg className="project-form-button-icons" viewBox="-8 -8 160 160"><g><g><rect x="127.026" y="103.44" transform="matrix(0.8661 0.4999 -0.4999 0.8661 69.9909 -49.9074)" width="2.251" height="4.499" /><path d="M122.188,104.845l-3.863-2.23l2.25-3.896l3.863,2.23L122.188,104.845z M114.462,100.383l-3.863-2.23    l2.25-3.896l3.863,2.23L114.462,100.383z M106.734,95.921l-3.864-2.23l2.25-3.896l3.864,2.23L106.734,95.921z M99.007,91.459    l-3.863-2.23l2.25-3.896l3.863,2.23L99.007,91.459z M91.28,86.997l-3.864-2.23l2.25-3.896l3.864,2.23L91.28,86.997z     M83.552,82.535l-3.863-2.23l2.25-3.896l3.863,2.23L83.552,82.535z" /><polygon points="75.825,78.073 75.001,77.598 74.177,78.073 71.927,74.177 75.001,72.402 78.075,74.177   " /><path d="M27.812,104.843l-2.25-3.896l3.863-2.231l2.251,3.896L27.812,104.843z M35.539,100.381l-2.25-3.896    l3.864-2.23l2.25,3.896L35.539,100.381z M43.267,95.919l-2.25-3.896l3.863-2.23l2.25,3.896L43.267,95.919z M50.995,91.458    l-2.251-3.896l3.864-2.231l2.25,3.896L50.995,91.458z M58.722,86.996l-2.25-3.896l3.864-2.23l2.25,3.896L58.722,86.996z     M66.45,82.534l-2.25-3.896l3.863-2.23l2.25,3.896L66.45,82.534z" /><rect x="19.599" y="104.562" transform="matrix(0.5001 0.866 -0.866 0.5001 102.4474 33.9166)" width="4.499" height="2.251" /></g><g><rect x="72.751" y="72.75" width="4.5" height="2.25" /><path d="M77.251,68.288h-4.5v-4.462h4.5V68.288z M77.251,59.365h-4.5v-4.462h4.5V59.365z M77.251,50.442h-4.5    V45.98h4.5V50.442z M77.251,41.519h-4.5v-4.461h4.5V41.519z M77.251,32.596h-4.5v-4.462h4.5V32.596z M77.251,23.673h-4.5v-4.462    h4.5V23.673z" /><rect x="72.751" y="12.5" width="4.5" height="2.25" /></g><g><path d="M75.001,140.098l-56.377-32.549V42.451L75.001,9.902l56.375,32.551v65.098L75.001,140.098z     M23.124,104.951l51.877,29.951l51.875-29.949V45.051L75.001,15.098L23.124,45.049V104.951z" /><polygon points="75.001,77.598 19.749,45.698 21.999,41.802 75.001,72.402 128.001,41.804 130.251,45.7   " /><rect x="72.751" y="75" width="4.5" height="62.5" /></g></g></svg>
+              </a>
+            </div>
           </DivWithCorners>
         </div>
 
