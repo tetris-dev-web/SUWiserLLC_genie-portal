a9cebb8d7ed9253c9cf97436cc702cb471513441
Liangtian Zhang <lzhang40@binghamton.edu>
2018-12-28 21:21:21 -0500

    added voting ratio to current voting cycle component

--- frontend/components/entities/dashboard/project_dashboard/votes_view/votes_view_2.jsx
+++ frontend/components/entities/dashboard/project_dashboard/votes_view/votes_view_2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import * as d3 from 'd3';
 import { connect } from 'react-redux';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return({
       pitchedProjects: [
         {
@@ -47,7 +47,6 @@ class VotesView2 extends React.Component{
     this.state={
 
     };
-    // this.setUpProject
   }
 
   componentDidMount(){
@@ -64,115 +63,127 @@ class VotesView2 extends React.Component{
       .attr('width', "100%")
       .attr('height', capitalRaised/24000)
       .attr('fill', '#aa7a60')
-      .style('opacity', .5)
+      .style('opacity', .5);
 
     this.setUp();
   }
 
   setUp(){
-    const { capitalRaised } = this.props
+    const { capitalRaised } = this.props;
     const projects = this.processProjectData();
 
     const valuationRect = this.svg.selectAll('.valuation-rect').remove();
     valuationRect.data(projects)
       .enter()
       .append('rect')
-      .attr('width', project => {
-        return `${project.projectWidth}%`
-      })
-      .attr('height', project => {
-        return (project.valuation - project.capitalRequired)/24000
-      })
-      .attr('fill', project => {
-        return capitalRaised < project.capitalRequired ? '#aa7a60' : '#61aba9';
-      })
-      .attr('x', project => {
-        return `${project.startX}%`;
-      })
-      .attr('y', project => {
-        return (capitalRaised - project.valuation)/24000;
-      })
+      .attr('width', project => `${project.projectWidth}%`)
+      .attr('height', project => project.projectValutionHeight)
+      .attr('fill', project => project.fill)
+      .attr('x', project => `${project.projectStartX}%`)
+      .attr('y', project => project.projectValutionStartY)
       .attr('opacity', .3)
       .style('stroke', 'white')
       .style('stroke-width', 2)
       .on('mouseover', this.handleMouseOver("valuation"))
-      .on('mouseout', this.handleMouseOut())
+      .on('mouseout', this.handleMouseOut());
 
-    const capRequiredRect = this.svg.selectAll('.cap-required-rect').remove();
+    const capRequiredRect = this.svg.selectAll('.capital-required-rect').remove();
     capRequiredRect.data(projects)
       .enter()
       .append('rect')
-      .attr('width', project => {
-        return `${project.projectWidth}%`;
-      })
-      .attr('height', project => {
-        return project.capitalRequired/24000
-      })
-      .attr('fill', project => {
-        return capitalRaised < project.capitalRequired ? '#aa7a60' : '#61aba9'
-      })
-      .attr('x', project => {
-        return `${project.startX}%`
-      })
-      .attr('y', project =>{
-        return (capitalRaised - project.capitalRequired)/24000
-      })
+      .attr('width', project => `${project.projectWidth}%`)
+      .attr('height', project => project.projectCapitalRequiredHeight)
+      .attr('fill', project => project.fill)
+      .attr('x', project => `${project.projectStartX}%`)
+      .attr('y', project => project.projectCapitalRequiredStartY)
       .attr('opacity', 1)
       .style('stroke', 'white')
       .style('stroke-width', 2)
       .on('mouseover', this.handleMouseOver("capitalRequired"))
       .on('mouseout', this.handleMouseOut());
 
+    const votePercentageRect = this.svg.selectAll('.vote-percentage-rect').remove();
+    votePercentageRect.data(projects)
+      .enter()
+      .append('rect')
+      .attr('width', project => `${project.projectWidth + 1}%`)
+      .attr('height', 30)
+      .attr('fill', project => project.fill)
+      .attr('x', project => `${project.projectStartX - .5}%`)
+      .attr('y', capitalRaised / 24000);
+
+    const votePercentageText = this.svg.selectAll('.vote-percentage-rect').remove();
+    votePercentageText.data(projects)
+      .enter()
+      .append('text')
+      .style("font-size", "18px")
+      .style("fill", '#000')
+      .style("text-anchor", "middle")
+      .text(project => `${project.voteShare * 100}%`)
+      .attr('x', project => `${project.projectRectCenter}%`)
+      .attr('y', capitalRaised / 24000 + 20);
   }
 
-   processProjectData(){
-     const { pitchedProjects } = this.props;
-     const numberOfProjects = this.props.pitchedProjects.length;
-     const percentOfScreen = 60;
-     const projectWidthPercentage = percentOfScreen - (numberOfProjects - 1);
-     let startX = 20;
-     return pitchedProjects.map(project => {
-       const projectWidth = project.voteShare * projectWidthPercentage;
-       const newProject = Object.assign({}, project, {
-         startX,
-         projectWidth
-       });
-       startX += (1 + projectWidth);
-       return newProject;
-     });
-   }
+  processProjectData(){
+    const { pitchedProjects, capitalRaised } = this.props;
+    const numberOfProjects = this.props.pitchedProjects.length;
+    const percentOfScreen = 60;
+    const projectWidthPercentage = percentOfScreen - (numberOfProjects - 1);
+    let projectStartX = 20;
+
+    return pitchedProjects.map(project => {
+      const projectWidth = project.voteShare * projectWidthPercentage;
+      const newProject = Object.assign({}, project, {
+        projectStartX,
+        projectWidth,
+        fill: capitalRaised < project.capitalRequired ? '#aa7a60' : '#61aba9',
+        projectValutionHeight: (project.valuation - project.capitalRequired) / 24000,
+        projectValutionStartY: (capitalRaised - project.valuation) / 24000,
+        projectCapitalRequiredHeight: project.capitalRequired / 24000,
+        projectCapitalRequiredStartY: (capitalRaised - project.capitalRequired) / 24000,
+        projectRectCenter: projectStartX + projectWidth / 2
+      });
+      projectStartX += (1 + projectWidth);
+      return newProject;
+    });
+  }
 
   handleMouseOver(type){
     const { capitalRaised } = this.props;
+    const pixelPerDigit = 3;
     return project => {
       this.svg.append('text')
-      .classed('svg-text', true)
-      .style("font-size", "12px")
-      .style("fill", 'white')
-      .text(project[type])
-      .attr('x',() => {
-        return (project.startX + project.projectWidth/2)/100 * window.innerWidth - (3 * String(project[type]).length);
-      } )
-      .attr('y', () => {
-        if (type === 'valuation') {
-          return (capitalRaised - project.valuation)/24000 + 15;
-        }
-        else {
-          return (capitalRaised - project.capitalRequired)/24000 + 15;
-        }
-      })
-      .attr('pointer-events', 'none');
+        .classed('svg-text', true)
+        .style("font-size", "12px")
+        .style("fill", () => type === 'capitalRequired' ? 'white' : project.fill)
+        .style("text-anchor", "middle")
+        .text(project[type])
+        .attr('x', () => `${project.projectRectCenter}%`)
+        .attr('y', () => type === 'valuation' ? (capitalRaised - project.valuation) / 24000 + 15 : (capitalRaised - project.capitalRequired) / 24000 + 15)
+        .attr('pointer-events', 'none');
+
+      this.svg.append('text')
+        .classed('svg-text', true)
+        .style("font-size", "12px")
+        .style("fill", () => type === 'capitalRequired' ? 'white' : project.fill)
+        .style("text-anchor", "middle")
+        .text(() => type === 'valuation' ? 'valuation' : 'capital needs')
+        .attr('x', () => type === 'valuation' ? 
+          `${project.projectRectCenter}%`:
+          `${project.projectRectCenter}%`)
+        .attr('y', () => type === 'valuation' ? (capitalRaised - project.valuation) / 24000 - 10 : (capitalRaised - project.capitalRequired) / 24000 - 10)
+        .attr('pointer-events', 'none');
     };
   }
 
   handleMouseOut(){
-    return () => d3.select('.svg-text').remove();
+    return () => d3.selectAll('.svg-text').remove();
   }
 
   render(){
-    const { maxValuation, capitalRaised } = this.props
+    const { maxValuation, capitalRaised } = this.props;
     return(
-      <div className="votes-view" style={{marginTop: (maxValuation - capitalRaised)/24000}}></div>
+      <div className="votes-view" style={{marginTop: (maxValuation - capitalRaised) / 24000 + 20}}></div>
     );
   }
 }
