ca78319d308427a334a74fa9a0d857ace62648a8
Liangtian Zhang <lzhang40@binghamton.edu>
2019-01-30 14:34:51 -0500

    ignored truffle folder

--- truffle/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var Token = artifacts.require("./Token.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-  // deployer.deploy(Token);
-};
