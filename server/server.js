438f8310c529b06639afdf6837e5379d57dae1f5
sergeim28 <sergiymisyura28@gmail.com>
2022-02-17 16:38:11 +0200

    fix package json

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const ngrok = require('ngrok');
-const decodeJWT = require('did-jwt').decodeJWT;
-const { Credentials } = require('uport-credentials');
-const transports = require('uport-transports').transport;
-const message = require('uport-transports').message.util;
-const { asyncMiddleware } = require('./middlewares/async_middleware');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const ngrok = require("ngrok");
+const decodeJWT = require("did-jwt").decodeJWT;
+const { Credentials } = require("uport-credentials");
+const transports = require("uport-transports").transport;
+const message = require("uport-transports").message.util;
+const { asyncMiddleware } = require("./middlewares/async_middleware");
 const {
   fetchProjects,
   fetchProjectModuleData,
   fetchProjectGraphData,
   demoInvestorVotesByProject,
   demoDepositCashflow,
-} = require('./controllers/projects_controller');
+} = require("./controllers/projects_controller");
 const {
   fetchTokenHistoryWithEarnings,
   fetchInvestorBalance,
   activateDemoInvestorPending,
   fetchEndTime,
-} = require('./controllers/token_controller');
-const { fetchWeiRaised, fetchPurchases, buyTokens } = require('./controllers/crowdsale_controller');
-const { voteAndUpdateProjects } = require('./controllers/voting_controller');
+} = require("./controllers/token_controller");
+const { fetchWeiRaised, fetchPurchases, buyTokens } = require("./controllers/crowdsale_controller");
+const { voteAndUpdateProjects } = require("./controllers/voting_controller");
 
-const { pitchProject, fetchStartTime } = require('./controllers/project_factory_controller');
-const { demoInvestorFreeVotes } = require('./controllers/voting_token_controller');
-const { collectDemoInvestorDividend } = require('./controllers/dividends_controller');
-const safeStringify = require('json-stringify-safe');
+const { pitchProject, fetchStartTime } = require("./controllers/project_factory_controller");
+const { demoInvestorFreeVotes } = require("./controllers/voting_token_controller");
+const { collectDemoInvestorDividend } = require("./controllers/dividends_controller");
+const safeStringify = require("json-stringify-safe");
 
 const {
   _projectInstance,
@@ -39,17 +39,32 @@ const {
   projectLeaderTrackerInstance,
   votingInstance,
   votingTokenInstance,
-} = require('./chain_models/models');
+} = require("./chain_models/models");
 
 const port = process.env.PORT || 8080;
 
-let endpoint = '';
+let endpoint = "";
 const app = express();
 
-app.use(bodyParser.json({ type: '*/*' }));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")),
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(bodyParser.json({ type: "*/*" }));
+app.get("/api/helloworld", (req, res) => {
+  res.send("API is working. hello world");
+});
 
 app.get(
-  '/api/fetchStartAndEndTimes',
+  "/api/fetchStartAndEndTimes",
   asyncMiddleware(async (req, res) => {
     const startTime = await fetchStartTime();
     const endTime = await fetchEndTime();
@@ -59,7 +74,7 @@ app.get(
 );
 
 app.get(
-  '/api/contract_instances',
+  "/api/contract_instances",
   asyncMiddleware(async (req, res) => {
     const eventSubscription = (callBack) => {
       return async () => {
@@ -74,7 +89,7 @@ app.get(
     };
 
     const func = eventSubscription(() => {
-      console.log('hello');
+      console.log("hello");
     });
     const the = await func();
     // console.log(the)
@@ -84,7 +99,7 @@ app.get(
 );
 
 app.get(
-  '/api/shared_project_graph_data',
+  "/api/shared_project_graph_data",
   asyncMiddleware(async (req, res) => {
     const projects = await fetchProjects();
     const weiRaised = await fetchWeiRaised();
@@ -93,7 +108,7 @@ app.get(
 );
 
 app.get(
-  '/api/project_graph_data/:address',
+  "/api/project_graph_data/:address",
   asyncMiddleware(async (req, res) => {
     const { address } = req.params;
     const project = await fetchProjectGraphData(address);
@@ -102,7 +117,7 @@ app.get(
 );
 
 app.get(
-  '/api/capital_history_data',
+  "/api/capital_history_data",
   asyncMiddleware(async (req, res) => {
     const _capitalHistoryData = await fetchPurchases();
     res.send(_capitalHistoryData);
@@ -110,7 +125,7 @@ app.get(
 );
 
 app.post(
-  '/api/token_graph_data',
+  "/api/token_graph_data",
   asyncMiddleware(async (req, res) => {
     const { body } = req;
     const { currentViewType, account } = body;
@@ -120,7 +135,7 @@ app.post(
 );
 
 app.get(
-  '/api/demo/demoInvestorFreeVotes',
+  "/api/demo/demoInvestorFreeVotes",
   asyncMiddleware(async (req, res) => {
     const freeVotes = await demoInvestorFreeVotes();
     res.send(freeVotes);
@@ -128,7 +143,7 @@ app.get(
 );
 
 app.get(
-  '/api/demo/project_votes/:projectAddress',
+  "/api/demo/project_votes/:projectAddress",
   asyncMiddleware(async (req, res) => {
     const { projectAddress } = req.params;
     const votes = await demoInvestorVotesByProject(projectAddress);
@@ -137,9 +152,9 @@ app.get(
 );
 
 app.post(
-  '/api/demo/vote_and_update_projects',
+  "/api/demo/vote_and_update_projects",
   asyncMiddleware(async (req, res) => {
-    console.log('VOTING');
+    console.log("VOTING");
     const { votes, type, selectedProject } = req.body;
     await voteAndUpdateProjects(votes, type, selectedProject);
     res.send({});
@@ -147,7 +162,7 @@ app.post(
 );
 
 app.post(
-  '/api/demo/pitch_project',
+  "/api/demo/pitch_project",
   asyncMiddleware(async (req, res) => {
     const { params } = req.body;
     await pitchProject(params);
@@ -156,7 +171,7 @@ app.post(
 );
 
 app.post(
-  '/api/demo/buy_tokens',
+  "/api/demo/buy_tokens",
   asyncMiddleware(async (req, res) => {
     const { wei } = req.body;
     await buyTokens(wei);
@@ -165,7 +180,7 @@ app.post(
 );
 
 app.get(
-  '/api/demo/fetch_investor_balance',
+  "/api/demo/fetch_investor_balance",
   asyncMiddleware(async (req, res) => {
     const balance = await fetchInvestorBalance();
     res.send(balance);
@@ -173,7 +188,7 @@ app.get(
 );
 
 app.post(
-  '/api/demo/deposit_cashflow',
+  "/api/demo/deposit_cashflow",
   asyncMiddleware(async (req, res) => {
     const { weiAmount, projectAddress } = req.body;
     await demoDepositCashflow(weiAmount, projectAddress);
@@ -182,7 +197,7 @@ app.post(
 );
 
 app.get(
-  '/api/demo/activate_investor_pending',
+  "/api/demo/activate_investor_pending",
   asyncMiddleware(async (req, res) => {
     await activateDemoInvestorPending();
     res.send({});
@@ -190,20 +205,20 @@ app.get(
 );
 
 app.get(
-  '/api/demo/collect_investor_dividend',
+  "/api/demo/collect_investor_dividend",
   asyncMiddleware(async (req, res) => {
     await collectDemoInvestorDividend();
     res.send({});
   }),
 );
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
 
 const server = app.listen(port, () => {
-  console.log('listening on port', port);
+  console.log("listening on port", port);
 });
