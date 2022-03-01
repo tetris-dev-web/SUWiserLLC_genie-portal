c86a553f0ad0f3d021d74ca2cf77e7891d015fe8
tetris.dev.web <tetris.dev.web@gmail.com>
2022-03-02 01:14:25 +0800

    contract addresses

---
+++ server/chain_models/contract_addresses.js
@@ -0,0 +1,11 @@
+module.exports = {
+  activationAddress: "0x24f9fbc3f422d7193adabc72ccca7552c769dcd0",
+  activeTokenAddress: "0xb22ae0d2c13fc636da8316f02a520e12bfe66677",
+  crowdsaleAddress: "0x6c2d5196f6824141dd35743e5154066148cfc0bf",
+  dividendsAddress: "0x1b7877973ea6089ae985c4feb3220a4aa0b6fd52",
+  inactiveTokenAddress: "0xa367e57f14f3a3071f0bf2b6b8b6388ff497c56c",
+  projectFactoryAddress: "0xbb1c3935a7cd94af500351b4ca9ddfc0d9061d38",
+  projectLeaderTrackerAddress: "0x2c867dcadb1c4090e823740d9ca677725e8cc434",
+  votingAddress: "0x0a0085555487ad51b1fad2886cf872091c6f0fcd",
+  votingTokenAddress: "0x88e8185f15cf9311c59592d9da4adb0a4f16587f"
+}
