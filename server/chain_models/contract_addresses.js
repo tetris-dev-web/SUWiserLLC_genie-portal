fb608ab02225be8bcbbcbeef85821d89b414176f
tetris.dev.web <tetris.dev.web@gmail.com>
2022-03-02 23:53:33 +0800

    fix barchart

--- server/chain_models/contract_addresses.js
+++ server/chain_models/contract_addresses.js
@@ -1,11 +1,11 @@
 module.exports = {
-  activationAddress: "0x24f9fbc3f422d7193adabc72ccca7552c769dcd0",
-  activeTokenAddress: "0xb22ae0d2c13fc636da8316f02a520e12bfe66677",
-  crowdsaleAddress: "0x6c2d5196f6824141dd35743e5154066148cfc0bf",
-  dividendsAddress: "0x1b7877973ea6089ae985c4feb3220a4aa0b6fd52",
-  inactiveTokenAddress: "0xa367e57f14f3a3071f0bf2b6b8b6388ff497c56c",
-  projectFactoryAddress: "0xbb1c3935a7cd94af500351b4ca9ddfc0d9061d38",
-  projectLeaderTrackerAddress: "0x2c867dcadb1c4090e823740d9ca677725e8cc434",
-  votingAddress: "0x0a0085555487ad51b1fad2886cf872091c6f0fcd",
-  votingTokenAddress: "0x88e8185f15cf9311c59592d9da4adb0a4f16587f"
+  activationAddress: "0x5a47a9612cea84d0a25fa9572f12a2249e154bfb",
+  activeTokenAddress: "0x09002ba16326a714128aee2318ac30dee7209728",
+  crowdsaleAddress: "0x48669c2b9fab0d54f5005250e58a6b56f5acde3d",
+  dividendsAddress: "0x5c4536d4a3bfa3cadb58cc6c39029b7dc8216631",
+  inactiveTokenAddress: "0x008157f886c8764f0b632b720c3c351f1e3a6406",
+  projectFactoryAddress: "0x3bd78e1256b0736fc09cc98df177a97447c4387c",
+  projectLeaderTrackerAddress: "0x206a7544d712a4aeedf3398c4a0b8f3ef2af67db",
+  votingAddress: "0x5202bc624d4018081c25c96b82e172d977673d1f",
+  votingTokenAddress: "0x27b1cafa83aa46d9f6e95e9704143a688c6eca7c"
 }
