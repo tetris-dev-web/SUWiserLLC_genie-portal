f001de867a534fabc259f7822f909b456ce942c5
tetris.dev.web <tetris.dev.web@gmail.com>
2022-03-24 01:34:06 +0800

    MongoDB Connection

--- server/db_util/profile.js
+++ server/db_util/profile.js
@@ -1,19 +1,6 @@
-const _database = 'genie-portal-nft';
-const _collection = 'profiles';
-const mongoose = require('mongoose');
-const { getProfileDataByEmail } = require('../controllers/profile_controller');
 const ProfileModel = require('./models/profile_model');
 
-const _connectMongoClusterClient = () => {
-  const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}.srdd2.mongodb.net/${_database}?retryWrites=true&w=majority`;
-  mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-      .then(() => console.log('MongoDB Database Connected'))
-      .catch(err => console.log(err)) 
-}
-
 const getData = async (condition) => {
-  await _connectMongoClusterClient();
-
   let newProfileInstance;
   await ProfileModel.findOne(condition, function (err, profile_instance) {
     if (err) return console.log(err);
@@ -24,8 +11,6 @@ const getData = async (condition) => {
 }
 
 const setData = async (data) => {
-  await _connectMongoClusterClient();
-
   var profile_instance = {
     first_name: data.firstName,
     middle_name: data.middleName,
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const path = require("path");
 const bodyParser = require("body-parser");
 // const ngrok = require("ngrok");
@@ -292,6 +293,11 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
   });
 }
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}.srdd2.mongodb.net/genie-portal-nft?retryWrites=true&w=majority`;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Database Connected'))
+    .catch(err => console.log(err));
 
 const server = app.listen(port, () => {
   console.log("listening on port", port);
