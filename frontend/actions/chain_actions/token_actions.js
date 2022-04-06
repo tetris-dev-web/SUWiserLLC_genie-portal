36c4ae2d40bb1af8e340a6739583c032d0eb1ec9
tetris.dev.web <tetris.dev.web@gmail.com>
2022-04-06 22:46:07 +0800

    FE : actions

--- frontend/actions/chain_actions/token_actions.js
+++ frontend/actions/chain_actions/token_actions.js
@@ -89,6 +89,14 @@ export const fetchDemoInvestorBalances = () => {
   return ExpressAPI.fetchApiData("demo/fetch_investor_balance");
 };
 
+export const fetchTokenHistory = (account) => {
+  return ExpressAPI.fetchApiData(`investor/token_history/${account}`);
+}
+
+export const fetchPurchaseHistory = (account) => {
+  return ExpressAPI.fetchApiData(`investor/purchase_history/${account}`);
+}
+
 export const receiveTokenTransfer = (tokenTransfer) => {
   return {
     type: RECEIVE_TOKEN_TRANSFER,
